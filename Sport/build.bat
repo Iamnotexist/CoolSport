1b1d3927e55753c687b17775c54e0718bc7f1328
Artemii Morozov <53973166+Iamnotexist@users.noreply.github.com>
2020-08-02 10:27:26 +0300

    Add files via upload

---
+++ Sport/build.bat
@@ -0,0 +1,8 @@
+g++ lib\date.cpp -c -o lib\date.o
+g++ lib\provod2.cpp -c -o lib\provod2.o
+g++ Sport.cpp -c -o Sport.o
+g++ -o Sport.exe Sport.o lib\date.o lib\provod2.o
+del /Q lib\date.o
+del /Q lib\provod2.o
+del /Q Sport.o
+Sport.exe
